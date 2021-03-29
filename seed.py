0833bbc7b63fc9d496889b56fa4f73805e748201
Jeff Hall <jeff.leon.hall@gmail.com>
2021-03-29 15:15:17 -0700

    modified seed file

--- seed.py
+++ seed.py
@@ -2,10 +2,10 @@ from models import db, Pet
 from app import app
 
 #Create pets
-cornelius = Pet('Cornelius', 'dog', age = 15, notes = 'Very good dog')
-sandra = Pet('Sandra', 'dog', age = 7, notes = 'Also a very good dog')
-matthew = Pet('Matthew', 'cat', age = 15, notes = 'A devious cat')
-bubbles = Pet('Bubbles', 'dolphin', age = 67, notes = 'A wise and happy dolphin', available = False,
+cornelius = Pet(name = 'Cornelius', species = 'dog', age = 15, notes = 'Very good dog')
+sandra = Pet(name = 'Sandra', species = 'dog', age = 7, notes = 'Also a very good dog')
+matthew = Pet(name = 'Matthew', species = 'cat', age = 15, notes = 'A devious cat')
+bubbles = Pet(name = 'Bubbles', species = 'dolphin', age = 67, notes = 'A wise and happy dolphin', available = False,
             photo_url = 'https://images.unsplash.com/photo-1570481662006-a3a1374699e8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=802&q=80')
 
 #Create all tables
