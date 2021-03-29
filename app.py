807e31ec18f767098d0f7af988315d5497b14d63
Jeff Hall <jeff.leon.hall@gmail.com>
2021-03-29 14:51:00 -0700

    added basic boilerplate

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, session, render_template, flash, redirect, request
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+connect_db(app)
+
+app.config['SECRET_KEY'] = '3m7z9n2x'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+
