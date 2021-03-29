052f88c3e8ac4d50b449d626d56632640628136c
Jeff Hall <jeff.leon.hall@gmail.com>
2021-03-29 14:59:18 -0700

    made the Pet model

--- models.py
+++ models.py
@@ -8,4 +8,13 @@ def connect_db(app):
 
 class Pet(db.Model):
     """Make a table of pets."""
-    
+
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, autoincrement = True. primary_key = True)
+    name = db.Column(db.Text, nullable = False)
+    species = db.Column(db.Text, nullable = False)
+    photo_url = db.Column(db.Text)
+    age = db.Column(db.Integer)
+    notes = db.Column(db.Text)
+    available = db.Column(db.Boolean, nullable = False, default = True)
